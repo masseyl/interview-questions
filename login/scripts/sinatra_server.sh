6a27a2cf9f0567fabaf60863651f7987c7308813
Lance Massey <lancemassey@Macintosh.local>
2013-05-19 22:16:21 -0400

    Removing older files

--- login/scripts/sinatra_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "==========================="
-echo "Running the Sinatra server at login/login.rb"
-echo "Use Ctrl + C to stop the server"
-echo "==========================="
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-ruby $DIR/../login.rb
